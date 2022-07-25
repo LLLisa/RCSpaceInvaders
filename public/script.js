4aad3cba412ff0e56b959ea9ef96831f0b67ee6b
Lisa Knox <lisaknox83@gmail.com>
2022-07-25 18:23:36 -0400

    invader movement done, death screen done

--- public/script.js
+++ public/script.js
@@ -8,14 +8,85 @@ const twotwo = document.getElementById('twotwo');
 const twothree = document.getElementById('twothree');
 const twofour = document.getElementById('twofour');
 const twofive = document.getElementById('twofive');
+const youDied = document.getElementById('youdied');
 
-oneone.classList.add('top0left0');
-onetwo.classList.add('top0left20');
-onethree.classList.add('top0left40');
-onefour.classList.add('top0left60');
-onefive.classList.add('top0left80');
-twoone.classList.add('top10left0');
-twotwo.classList.add('top10left20');
-twothree.classList.add('top10left40');
-twofour.classList.add('top10left60');
-twofive.classList.add('top10left80');
+const invaders = [
+  oneone,
+  onetwo,
+  onethree,
+  onefour,
+  onefive,
+  twoone,
+  twotwo,
+  twothree,
+  twofour,
+  twofive,
+];
+
+let currentX = 0,
+  currentY = 0;
+currentTimeout = 0;
+
+const frameDelay = 500;
+const framesPerTraversal = 12;
+const timeoutInterval = frameDelay * framesPerTraversal;
+
+const positions = (array, x = 0, y = 0) => {
+  for (let i = 0; i < array.length; i++) {
+    array[i].classList.add('invader');
+    array[i].style.position = 'absolute';
+    array[i].style.left = i < 5 ? `${y + i * 15}%` : `${y + (i - 5) * 15}%`;
+    array[i].style.top = i < 5 ? `${x}%` : `${x + 8}%`;
+  }
+};
+
+const moveRight = (x = 0, y = 0) => {
+  for (let i = 0; i < framesPerTraversal; i++) {
+    setTimeout(() => {
+      positions(invaders, x, y);
+      currentY = y;
+      y += 3;
+    }, i * frameDelay);
+  }
+};
+
+const moveDown = (x = 0, y = 0) => {
+  x += 10;
+  positions(invaders, x, y);
+  currentX = x;
+};
+
+const moveLeft = (x = 0, y = 0) => {
+  for (let i = 0; i < framesPerTraversal; i++) {
+    setTimeout(() => {
+      positions(invaders, x, y);
+      currentY = y;
+      y -= 3;
+    }, i * frameDelay);
+  }
+};
+
+const animation = async (array) => {
+  for (let i = 0; i < 4; i++) {
+    setTimeout(() => {
+      moveRight(currentX, currentY);
+    }, currentTimeout);
+    currentTimeout += timeoutInterval;
+    setTimeout(() => {
+      moveDown(currentX, currentY);
+    }, currentTimeout);
+    setTimeout(() => {
+      moveLeft(currentX, currentY);
+    }, currentTimeout);
+    currentTimeout += timeoutInterval;
+    setTimeout(() => {
+      if (currentX > 69) {
+        youDied.style.opacity = '100%';
+      }
+      moveDown(currentX, currentY);
+    }, currentTimeout);
+  }
+};
+
+positions(invaders);
+animation();
